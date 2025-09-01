a64fa55b8504f4bae7cdf359ab87eb8cfec3068c
Achille <achille.aaron.loche@gmail.com>
2025-09-01 10:14:55 +0200

    Added better choice system and prime factors

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    static int number;
+    static int toDo;
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        FindPrimeFactors primeFactors = new FindPrimeFactors();
+
+        while (true){
+
+            System.out.println("Tell what to do:");
+            System.out.println("Option 0: Count primes");
+            System.out.println("Option 1: Find prime factors");
+            toDo = Integer.parseInt(scanner.nextLine());
+
+            switch(toDo){
+                case 0:
+                    System.out.println("Count primes up to:");
+                    number = Integer.parseInt(scanner.nextLine());
+
+                    startSieve(number);
+                    System.out.println(" ");
+                    break;
+                case 1:
+                    System.out.println("Find prime factors of:");
+                    number = Integer.parseInt(scanner.nextLine());
+
+                    primeFactors.findAndPrintPrimeFactors(number);
+                    break;
+                default:
+                    System.out.println("Invalid thing to do");
+            }
+
+
+
+        }
+
+    }
+
+    public static void startSieve(int n){
+
+        SieveOfErathosteles sieve = new SieveOfErathosteles();
+
+        long startTime = System.nanoTime();
+        System.out.println("Primes found: " + sieve.Sieve(n));
+        long endTime = System.nanoTime();
+
+        double duration = (endTime - startTime) / 1_000_000_000.0;
+        System.out.println("Time taken: " + duration);
+    }
 }
--- src/SieveOfErathosteles.java
+++ src/SieveOfErathosteles.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class SieveOfErathosteles {
+
+    int count;
+
+
+    public int Sieve(int n){
+
+        boolean[] primeNumbers = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++){
+            primeNumbers[i] = true;
+        }
+
+        primeNumbers[0] = false;
+        primeNumbers[1] = false;
+
+        for (int numberSieving = 2; numberSieving*numberSieving <= n; numberSieving++){
+            if(primeNumbers[numberSieving]){
+                for (int multipleOfNumber = numberSieving*numberSieving; multipleOfNumber <= n; multipleOfNumber += numberSieving){
+                    primeNumbers[multipleOfNumber] = false;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 2; i <= n; i++){
+            if(primeNumbers[i]){
+                count += 1;
+            }
+        }
+        return count;
+    }
 }
